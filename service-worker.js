4ef95e8dd4a5623ab41edabcd4798cb36196c4fc
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-04-21 11:16:16 -0700

    added service-worker installation and event listeners

---
+++ service-worker.js
@@ -0,0 +1,67 @@
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./events.html",
+  "./tickets.html",
+  "./schedule.html",
+  "./assets/css/style.css",
+  "./assets/css/bootstrap.css",
+  "./assets/css/tickets.css",
+  "./dist/app.bundle.js",
+  "./dist/events.bundle.js",
+  "./dist/tickets.bundle.js",
+  "./dist/schedule.bundle.js",
+];
+const APP_PREFIX = "FoodFest-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
+
+//self initiates the listeners
+//use e.waitUnitl to tell browser to wait until work is compelte
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    //caches.open to find specific caceh by name
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+//keys return an array of all cache names
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,53 @@
 const path = require("path");
 //import webpack because we plan to use plugin in module exports
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 
 //create a main configuration entry, output and mode
-module.exports = {
-    entry: './assets/js/script.js',
+const config  = {
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+      },      
     output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use:[
+            {
+                loader: 'file-loader',
+                options:{//name returns the name of the file with the file ext
+                    name (file) {
+                        return "[path][name].[ext]"
+                    },//changes our assignment url by replacing ../ with /assets/
+                    publicPath:function(url) {
+                        return url.replace("../","/assets")
+                    }
+                }
+            },{
+                loader:'image-webpack-loader'
+            }
+        ]//end of use
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
         $: "jquery",
         jQuery: "jquery"
       }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      })
   ],
   mode: 'development'
 };
+
+module.exports = config;
